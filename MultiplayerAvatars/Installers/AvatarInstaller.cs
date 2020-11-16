b1da86396a15cf6795577aea789aa0e017a3ed53
MT <littlehappytroll@gmail.com>
2020-11-16 11:34:01 -0500

    installer patch, thanks auros

--- MultiplayerAvatars/Installers/AvatarInstaller.cs
+++ MultiplayerAvatars/Installers/AvatarInstaller.cs
@@ -14,21 +14,6 @@ namespace MultiplayerAvatars.Installers
         public override void InstallBindings()
         {
             Plugin.Log?.Info("Injecting Dependencies");
-
-            if (IPA.Loader.PluginManager.GetPluginFromId("MultiplayerExtensions") != null)
-            {
-                Plugin.Log?.Info("Found MultiplayerExtensions");
-
-                if (IPA.Loader.PluginManager.GetPluginFromId("CustomAvatar") != null)
-                {
-                    BindCustomAvatars(Container);
-                }
-            }
-        }
-
-        private void BindCustomAvatars(DiContainer Container)
-        {
-            Plugin.Log?.Info("Found CustomAvatar");
             Container.BindInterfacesAndSelfTo<ModelSaber>().AsSingle();
             Container.Bind(typeof(IInitializable), typeof(CustomAvatarManager)).To<CustomAvatarManager>().AsSingle();
         }
